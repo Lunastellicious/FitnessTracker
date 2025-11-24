eafab4479cd92613cb567a5f4d64cfc86c1276bb
Lunastellicious <158474694+Lunastellicious@users.noreply.github.com>
2025-11-24 13:46:00 +0100

    oprettelse af linear_regression.c og header

--- Src/core/linear_regression.c
+++ Src/core/linear_regression.c
@@ -3,3 +3,10 @@
 //
 
 #include "../include/linear_regression.h"
+
+
+static double X[1000] = { }; // input
+static double y[1000] = { }; //output
+static double weight = 0;
+static double bias = 0;
+
