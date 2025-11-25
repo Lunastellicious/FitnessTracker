f3feecd4903eff5f33b61add682542137784a0e3
Lunastellicious <158474694+Lunastellicious@users.noreply.github.com>
2025-11-25 16:06:34 +0100

    oprettelse af linear_regression.c og header

--- Src/core/linear_regression.c
+++ Src/core/linear_regression.c
@@ -3,6 +3,8 @@
 //
 
 #include "../include/linear_regression.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct
 {
@@ -15,17 +17,23 @@ typedef struct
 
     //training/test split
     int train_size; //80%
+    double* x_train;
+    double* VO2max_train;
     int test_size; //20%
+    double* x_test;
+    double* VO2max_test;
 } dataSet;
 
 static double* predict (double inputs[], int size, double weight, double bias);
 void split_datasets(dataSet* data, dataSet* dataSets, double train_ratio);
+void split_datasettester(dataSet* data, double train_ratio); //tester til et dataset
 
-int main ()
+
+int machineLearning ()
 {
     //age data
-    double age_x[] = {1, 2, 4, 5, 6, 7, 8, 9, 10};
-    double VO2_age[] = {1, 2, 4, 5, 6, 7, 8, 9, 10};
+    double age_x[] = {};
+    double VO2_age[] = {};
     dataSet age_data = {
         .x = age_x,
         .VO2max = VO2_age,
@@ -96,35 +104,105 @@ int main ()
     };
 
     //training hours
-    double trainingHours_x[] = {};
-    double VO2_trainingHours[] = {};
+    double trainingHours_x[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    double VO2_trainingHours[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     dataSet trainingHours_data = {
         .x = trainingHours_x,
         .VO2max = VO2_trainingHours,
-        .size = 1000,
+        .size = 10,
         .name = "training hours vs. V02max",
         .weight = 0,
         .bias = 0
     };
 
-    //array med alle datasets
     dataSet dataSets [] = {age_data, weight_data, restingHeartRate_data, maxHeartRate_data, cumulativeHits_data, cumulativeRuns_data, trainingHours_data};
     int num_dataSets = 7;
-    split_datasets(dataSets, dataSets, 0.8);
-}
 
-//ændre input ved funktionskald
-
-static double* predict (double inputs[], int size, double weight, double bias);
+    split_datasets(&dataSets, dataSets, 0.8);
+    printf("training data: \n");
 
 
-void split_datasets(dataSet* data, dataSet* dataSets, double train_ratio) { //splitter dataset op i 2 dele
+    /*
+    split_datasettester(&trainingHours_data, 0.8);
 
-
-    for (int data_size = 0; data_size < (sizeof(dataSets) / sizeof(dataSet)); data_size++)
+    printf("training data: \n");
+    for (int i = 0; i < trainingHours_data.train_size; i++)
     {
-        dataSets[data_size].train_size = (int)(data->size * train_ratio); // 80%
-        dataSets[data_size].test_size = data->size - data->train_size; // 20%
+        printf("%1.lf %1.lf \n ", trainingHours_data.x_train[i], trainingHours_data.VO2max_train[i]); //train data
     }
 
+    printf("test data: \n");
+    for (int i = 0; i < trainingHours_data.test_size; i++)
+    {
+        printf("%1.lf %1.lf \n ", trainingHours_data.x_test[i], trainingHours_data.VO2max_test[i]); //test data
+    }
+    */
+
+    //array med alle datasets
+
+
+    // Frigør hukommelse når du er færdig!
 }
+
+//// FUNCTIONS
+
+void split_datasettester(dataSet* data, double train_ratio) {
+    // Beregner størrelser - brug data->size
+    data->train_size = (int)(data->size * train_ratio);
+    data->test_size = data->size - data->train_size;
+
+    // Laver hukommelse til training data
+    data->x_train = (double*)malloc(data->train_size * sizeof(double));
+    data->VO2max_train = (double*)malloc(data->train_size * sizeof(double));
+
+    // Laver hukommelse til test data
+    data->x_test = (double*)malloc(data->test_size * sizeof(double));
+    data->VO2max_test = (double*)malloc(data->test_size * sizeof(double));
+
+    // Kopiere data til training data (80%) - brug x_TRAIN
+    for (int i = 0; i < data->train_size; i++) {
+        data->x_train[i] = data->x[i];
+        data->VO2max_train[i] = data->VO2max[i];
+    }
+
+    // Kopiere data til test data (20%) - start fra train_size
+    for (int i = 0; i < data->test_size; i++) {
+        data->x_test[i] = data->x[data->train_size + i];
+        data->VO2max_test[i] = data->VO2max[data->train_size + i];
+    }
+}
+
+void split_datasets(dataSet* data, dataSet* dataSets, double train_ratio)
+{
+    //splitter datasets op i 2 dele
+    for (int data_size = 0; data_size < (sizeof(dataSets) / sizeof(dataSet)); data_size++)
+    {
+        // Beregner størrelser - brug data->size
+        data->train_size = (int)(data->size * train_ratio);
+        data->test_size = data->size - data->train_size;
+
+        // Laver hukommelse til training data
+        data->x_train = (double*)malloc(data->train_size * sizeof(double));
+        data->VO2max_train = (double*)malloc(data->train_size * sizeof(double));
+
+        // Laver hukommelse til test data
+        data->x_test = (double*)malloc(data->test_size * sizeof(double));
+        data->VO2max_test = (double*)malloc(data->test_size * sizeof(double));
+
+        // Kopiere data til training data (80%) - brug x_TRAIN
+        for (int i = 0; i < data->train_size; i++) {
+            data->x_train[i] = data->x[i];
+            data->VO2max_train[i] = data->VO2max[i];
+        }
+
+        // Kopiere data til test data (20%) - start fra train_size
+        for (int i = 0; i < data->test_size; i++) {
+            data->x_test[i] = data->x[data->train_size + i];
+            data->VO2max_test[i] = data->VO2max[data->train_size + i];
+        }
+
+    }
+}
+
+
+//TODO: predict function
--- Src/main.c
+++ Src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "./database/data.h"
+#include "./include/linear_regression.h"
 
 int main(void)
 {
     printf("Hello, World!\n");
+
+    machineLearning();
+
     return 0;
+
 }
