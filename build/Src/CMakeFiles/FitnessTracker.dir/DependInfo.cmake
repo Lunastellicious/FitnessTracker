a261d4a07f016d946b73d66e713df48dfe1cdf9d
Ruben <Nordsoe2003@proton.me>
2025-11-26 13:11:59 +0100

    VSC editions

---
+++ build/Src/CMakeFiles/FitnessTracker.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/Ruben/Coding/FitnessTracker/Src/core/linear_regression.c" "Src/CMakeFiles/FitnessTracker.dir/core/linear_regression.c.o" "gcc" "Src/CMakeFiles/FitnessTracker.dir/core/linear_regression.c.o.d"
+  "/home/Ruben/Coding/FitnessTracker/Src/core/logic.c" "Src/CMakeFiles/FitnessTracker.dir/core/logic.c.o" "gcc" "Src/CMakeFiles/FitnessTracker.dir/core/logic.c.o.d"
+  "/home/Ruben/Coding/FitnessTracker/Src/core/utils.c" "Src/CMakeFiles/FitnessTracker.dir/core/utils.c.o" "gcc" "Src/CMakeFiles/FitnessTracker.dir/core/utils.c.o.d"
+  "/home/Ruben/Coding/FitnessTracker/Src/database/data.c" "Src/CMakeFiles/FitnessTracker.dir/database/data.c.o" "gcc" "Src/CMakeFiles/FitnessTracker.dir/database/data.c.o.d"
+  "/home/Ruben/Coding/FitnessTracker/Src/main.c" "Src/CMakeFiles/FitnessTracker.dir/main.c.o" "gcc" "Src/CMakeFiles/FitnessTracker.dir/main.c.o.d"
+  "" "Src/FitnessTracker" "gcc" "Src/CMakeFiles/FitnessTracker.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
